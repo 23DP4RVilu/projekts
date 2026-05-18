63f4a776f9ed428a683b990624f62602d5659a19
23DP4RVilu <A230502RV@rvt.lv>
2026-05-18 22:40:38 +0300

    something something update

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173','https://your-frontend-url.up.railway.app',],
+    'allowed_origins' => ['http://localhost:5173','https://glistening-intuition-production-3728.up.railway.app',],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -1,12 +1,12 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://your-backend-url.up.railway.app/api',
+  baseURL: 'https://projekts-production.up.railway.app',
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json'
   },
-  withCredentials: false,
+  withCredentials: true,
 })
 
 export default api
